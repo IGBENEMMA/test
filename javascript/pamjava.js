ae1a5f7bfcedfa2376212dc9fe79f819b1cfcb68
IGBENEMMA <165777994+IGBENEMMA@users.noreply.github.com>
2025-02-24 23:31:23 +0100

    Add files via upload

---
+++ javascript/pamjava.js
@@ -0,0 +1,14 @@
+// function top() {
+// var pic = ["pexels-shvetsa-3852192.jpg", "pexels-delbeautybox-211032-705255.jpg"];
+// randompic = pic[Math.floor(Math.random() * pic.length)];
+// console.log(randompic)
+// document.getElementById("topimage").src = randompic.src
+// }
+
+
+// const changeBtn = document.getElementById('changeBtn')
+
+
+
+
+
